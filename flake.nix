6128b83e033886f542208fdf4878a30d63326c95
Alex Spaulding <aspauldingcode@gmail.com>
2026-07-31 17:01:41 -0700

    chore(flake): track toolchain development and lock tips

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    wwn-toolchain.url = "github:Wawona/wwn-toolchain";
+    wwn-toolchain.url = "github:Wawona/wwn-toolchain/development";
     wwn-toolchain.inputs.nixpkgs.follows = "nixpkgs";
     wwn-toolchain.inputs.rust-overlay.follows = "rust-overlay";
     # Graphics implementations (ANGLE/SwiftShader/ICDs) are owned by L1 iland.
